093b0615c5db73460b572b5b291e48b5da4be949
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-04-26 11:46:48 -0700

    Create hooks.client.ts

---
+++ src/hooks.client.ts
@@ -0,0 +1,6 @@
+// TODO: Add URL Search Params demo.
+//
+// Reference the following URLs to learn more about SvelteKit hooks:
+// - https://kit.svelte.dev/docs/hooks
+// - https://youtu.be/Kzrz7GZ9pIg
+//
